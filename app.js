d23359f8aa0c87900bb428fc698ed795be5a06d9
AbdulRehman <likeam99@gmail.com>
2022-05-12 20:08:54 +0500

    Building the seach form

--- app.js
+++ app.js
@@ -31,3 +31,11 @@ window.onscroll = () =>{
     });
 
 }
+
+document.querySelector('#search-icon').onclick = () =>{
+document.querySelector('#search-form').classList.toggle('active');
+}
+    
+document.querySelector('#close').onclick = () =>{
+document.querySelector('#search-form').classList.remove('active');
+}    
