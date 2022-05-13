c96513e002ef8b62f2f356e119e68921d162f127
AbdulRehman <likeam99@gmail.com>
2022-05-13 09:53:38 +0500

    Building the Home section

--- app.js
+++ app.js
@@ -39,3 +39,26 @@ document.querySelector('#search-form').classList.toggle('active');
 document.querySelector('#close').onclick = () =>{
 document.querySelector('#search-form').classList.remove('active');
 }    
+
+const swiper = new Swiper('.swiper', {
+   // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+  // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    },
+
+  // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+  // And if we need scrollbar
+    scrollbar: {
+    el: '.swiper-scrollbar',
+    },
+});
+    
