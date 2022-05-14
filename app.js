54e114cc84ecdb277829dbf43a44d73f47d2d033
AbdulRehman <likeam99@gmail.com>
2022-05-14 11:29:48 +0500

    Building the review section

--- app.js
+++ app.js
@@ -40,25 +40,48 @@ document.querySelector('#close').onclick = () =>{
 document.querySelector('#search-form').classList.remove('active');
 }    
 
-const swiper = new Swiper('.home-slider', {
-   // Optional parameters
-    direction: 'vertical',
-    loop: true,
+// const swiper = new Swiper('.home-slider', {
+//    // Optional parameters
+//     direction: 'vertical',
+//     loop: true,
 
-  // If we need pagination
-    pagination: {
-    el: '.swiper-pagination',
-    },
+//   // If we need pagination
+//     pagination: {
+//     el: '.swiper-pagination',
+//     },
 
-  // Navigation arrows
-    navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-    },
+//   // Navigation arrows
+//     navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//     },
 
-  // And if we need scrollbar
-    scrollbar: {
-    el: '.swiper-scrollbar',
-    },
-});
+//   // And if we need scrollbar
+//     scrollbar: {
+//     el: '.swiper-scrollbar',
+//     },
+// });
     
+let swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop:true,
+  breakpoints: {
+    0: {
+        slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
